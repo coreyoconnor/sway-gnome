9d3b2ab3d8a4829bb7a021352c715d940a65d490
Corey O'Connor <coreyoconnor@gmail.com>
2023-01-04 01:02:32 +0000

    add manual launch of polkit auth agent

--- default.nix
+++ default.nix
@@ -123,7 +123,28 @@ in {
           };
         };
 
-        systemd.packages = [ sway-gnome-desktop ];
+        systemd = {
+          packages = [ sway-gnome-desktop ];
+
+          user.services = {
+            polkit-gnome-authentication-agent-1 = {
+              unitConfig = {
+                Description = "polkit-gnome-authentication-agent-1";
+                Wants = [ "graphical-session.target" ];
+                WantedBy = [ "graphical-session.target" ];
+                After = [ "graphical-session.target" ];
+              };
+
+              serviceConfig = {
+                Type = "simple";
+                ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+                Restart = "on-failure";
+                RestartSec = 1;
+                TimeoutStopSec = 10;
+              };
+            };
+          };
+        };
       };
     };
 }
